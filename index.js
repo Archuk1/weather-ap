b50028abe1c16f61fb27d3b41ca20c999b708489
Archuk1 <vadimkiricenko572@gmail.com>
2025-12-01 23:21:25 +0000

    Deploying to gh-pages from @ Archuk1/weather-ap@f9fa28f847ab323696498bf08bde473748d8b03b 🚀

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-import{a as L,S as v,i as a}from"./assets/vendor-DeKi_rfE.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))p(t);new MutationObserver(t=>{for(const o of t)if(o.type==="childList")for(const u of o.addedNodes)u.tagName==="LINK"&&u.rel==="modulepreload"&&p(u)}).observe(document,{childList:!0,subtree:!0});function r(t){const o={};return t.integrity&&(o.integrity=t.integrity),t.referrerPolicy&&(o.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?o.credentials="include":t.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function p(t){if(t.ep)return;t.ep=!0;const o=r(t);fetch(t.href,o)}})();async function f(s,e){return(await L.get("https://pixabay.com/api/",{params:{key:"53395315-8c17e52e5e4d7b2c0ffc17b33",q:s,image_type:"photo",safesearch:!0,orientation:"horizontal",page:e,per_page:15}})).data}const l={gallery:document.querySelector(".gallery"),loader:document.querySelector(".loader"),loadMoreBtn:document.querySelector(".js-loadbtn")};let w=new v(".gallery a",{captionDelay:250,captionsData:"alt"});function g(s){const e=s.map(r=>`
-      <li class="gallery-item">
-        <a class="gallery-link" href="${r.largeImageURL}">
-          <img
-            class="gallery-image"
-            src="${r.webformatURL}"
-            alt="${r.tags}"
-          />
-        </a>
-        <div class="info">
-          <p><b>Likes</b> ${r.likes}</p>
-          <p><b>Views</b> ${r.views}</p>
-          <p><b>Comments</b> ${r.comments}</p>
-          <p><b>Downloads</b> ${r.downloads}</p>
-        </div>
-      </li>
-    `).join("");l.gallery.insertAdjacentHTML("beforeend",e),w.refresh()}function q(){l.gallery.innerHTML=""}function y(){l.loader.classList.remove("hidden")}function i(){l.loader.classList.add("hidden")}function h(){l.loadMoreBtn.classList.remove("hidden")}function d(){l.loadMoreBtn.classList.add("hidden")}const b={form:document.querySelector(".form"),loadMoreBtn:document.querySelector(".js-loadbtn")};let n,c,m;b.form.addEventListener("submit",async s=>{if(s.preventDefault(),d(),q(),y(),n=1,c=s.target.elements.searchText.value,c.trim()==="")return i(),a.error({message:"Please enter query!",position:"topRight"});try{const e=await f(c,n);if(m=Math.ceil(e.totalHits/15),e.totalHits==0)return i(),a.error({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"});g(e.hits),i(),n<m?h():a.info({message:"We're sorry, but you've reached the end of search results.",position:"topRight"})}catch(e){i(),d(),a.error({message:e.message,position:"topRight"})}s.target.reset()});b.loadMoreBtn.addEventListener("click",async()=>{n+=1,d();let e=document.querySelector(".gallery-item").getBoundingClientRect();try{y();const r=await f(c,n);g(r.hits),i(),window.scrollBy({top:e.height*2,behavior:"smooth"}),n<m?h():a.info({message:"We're sorry, but you've reached the end of search results.",position:"topRight"})}catch(r){i(),d(),a.error({message:r.message,position:"topRight"})}});
+import{a as c,i as a}from"./assets/vendor-Vjjz0Re7.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&i(n)}).observe(document,{childList:!0,subtree:!0});function t(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=t(e);fetch(e.href,r)}})();async function l(s,o){const i=`https://www.amdoren.com/api/weather.php?api_key=vcRK23EVDEbMBXvrVm8tmM73REsxVx&lat=${s}&lon=${o}`,e=`https://corsproxy.io/?${encodeURIComponent(i)}`;try{return(await c.get(e)).data}catch(r){throw console.error("Weather error:",r),r}}const d=document.querySelector(".weather-card");function u(s){const o=s.map(t=>`<div class="weather-card">
+  <div class="date">${t.date}</div>
+  <img src="${t.icon}" alt="Cloudy" class="icon">
+  <div class="summary">${t.summary}</div>
+  <ul class="temps">
+    <li>Min: ${t.min_c}°C</li>
+    <li>Max: ${t.max_c}°C</li>
+    <li>Avg: ${t.avg_c}°C</li>
+  </ul>
+</div>
+`).join("");return d.innerHTML=o}const m={form:document.querySelector(".js-form")};m.form.addEventListener("submit",async s=>{s.preventDefault();const o=new FormData(s.target),t=o.get("lat"),i=o.get("lon");try{if(t.trim()===""||i.trim()==="")return a.error({message:"Enter all fields",position:"topRight"});const e=await l(t,i);u(e.forecast)}catch(e){return a.error({message:e.message,position:"topRight"})}s.target.reset()});
 //# sourceMappingURL=index.js.map
